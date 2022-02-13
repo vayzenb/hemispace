e90963daffc094ea03298196b4352c035d758ac7
vayzenb <vayzenberg90@gmail.com>
2022-02-13 14:02:53 -0500

    fixed mirroring, added dorsal spread

--- register_mirror.py
+++ register_mirror.py
@@ -28,10 +28,12 @@ c_hemi = ['left', 'right']
 
 
 subj_list = ['hemispace1001','hemispace1002','hemispace1003', 'hemispace2001', 'hemispace2002', 'hemispace2003']
-subj_list = ['hemispace1001','hemispace1002','hemispace1003']
+#subj_list = ['hemispace1001','hemispace1002','hemispace1003']
 
-subj_list=['hemispace2001', 'hemispace2002', 'hemispace2003']
+#subj_list=['hemispace2001', 'hemispace2002', 'hemispace2003']
 
+#subj_list = ['hemispace1002']
+#p_hemi = ['left']
 
 #left is negative, right is positive
 mni = load_mni152_brain_mask()
@@ -40,10 +42,22 @@ parcel_mni='/opt/fsl/6.0.3/data/standard/MNI152_T1_1mm_brain.nii.gz' #this is th
 anat_mni='/opt/fsl/6.0.3/data/standard/MNI152_T1_2mm_brain.nii.gz' #this is the MNI we use for analysis
 parcel_root = "/user_data/vayzenbe/GitHub_Repos/fmri/roiParcels/"
 parcel_type = "mruczek_parcels/binary"
-parcels = ['PPC', 'APC']
+parcels = ['V3ab','PPC', 'APC']
+parcels = ['V3ab']
+
+#for wang parcels iterate through the numbers you want
+parcel_num = list(range(16,25))
+parcels = []
+for pn in parcel_num:
+    parcels.append(f'perc_VTPM_vol_roi{pn}_lh')
+    parcels.append(f'perc_VTPM_vol_roi{pn}_rh')
+
+print(parcels)
+
 
 #exp = 
 def create_mirror_brain(sub):
+
     print("creating brain mirror", sub[1])
     sub_dir = f'{study_dir}/sub-{sub[1]}/ses-01/'
     #stat_dir = f'{sub_dir}/fsl/{exp[1]}/HighLevel{suf}.gfeat/cope{copes[exp[0]]}.feat/'
@@ -59,17 +73,16 @@ def create_mirror_brain(sub):
     mid = list((np.array((hemi_mask.shape))/2).astype(int)) #find mid point of image
 
     hemi_mask[hemi_mask>0] = 1 #ensure to mask all of it
-
-    if p_hemi[sub[0]] == 'left':
-        hemi_mask[mid[0]:, :, :] = 0 
-    else:
-        hemi_mask[:mid[0], :, :] = 0 
-
     anat_flip = anat
     anat_mirror = anat
     anat_flip =anat_flip[::-1,:, :]
 
-    anat_mirror[:mid[0],:,:] = anat_flip[:mid[0],:,:]
+    if p_hemi[sub[0]] == 'left':
+        hemi_mask[mid[0]:, :, :] = 0 
+        anat_mirror[mid[0]:,:,:] = anat_flip[mid[0]:,:,:]
+    else:
+        hemi_mask[:mid[0], :, :] = 0 
+        anat_mirror[:mid[0],:,:] = anat_flip[:mid[0],:,:]
 
     anat_mirror = nib.Nifti1Image(anat_mirror, affine)  # create the volume image
     nib.save(anat_mirror,f'{sub_dir}/anat/sub-{sub[1]}_ses-01_T1w_brain_mirrored.nii.gz')
@@ -85,7 +98,9 @@ def register_mni(sub):
     if int(sub[1][-4:]) < 2000:
         anat_mirror = f'{anat_dir}/sub-{sub[1]}_ses-01_T1w_brain_mirrored.nii.gz'
     else:
-        anat_mirror = f'{anat_dir}/sub-{sub[1]}_ses-01_T1w_brain_brain.nii.gz'
+        anat_mirror = f'{anat_dir}/sub-{sub[1]}_ses-01_T1w_brain.nii.gz'
+    
+    
     anat = f'{anat_dir}/sub-{sub[1]}_ses-01_T1w_brain.nii.gz'
 
 
@@ -134,13 +149,13 @@ def register_parcels(sub, parcel_dir, parcels):
 
     for rp in parcels:
         
-        roi_parcel  = f'{parcel_dir}/l{rp}.nii.gz'
-        bash_cmd = f'flirt -in {roi_parcel} -ref {anat} -out {roi_dir}/parcels/l{rp}.nii.gz -applyxfm -init {anat_dir}/parcel2mirror.mat -interp trilinear'
+        roi_parcel  = f'{parcel_dir}/{rp}.nii.gz'
+        bash_cmd = f'flirt -in {roi_parcel} -ref {anat} -out {roi_dir}/parcels/{rp}.nii.gz -applyxfm -init {anat_dir}/parcel2mirror.mat -interp trilinear'
         subprocess.run(bash_cmd.split(), check = True)
 
-        roi_parcel  = f'{parcel_dir}/r{rp}.nii.gz'
-        bash_cmd = f'flirt -in {roi_parcel} -ref {anat} -out {roi_dir}/parcels/r{rp}.nii.gz -applyxfm -init {anat_dir}/parcel2mirror.mat -interp trilinear'
-        subprocess.run(bash_cmd.split(), check = True)
+        #roi_parcel  = f'{parcel_dir}/r{rp}.nii.gz'
+        #bash_cmd = f'flirt -in {roi_parcel} -ref {anat} -out {roi_dir}/parcels/r{rp}.nii.gz -applyxfm -init {anat_dir}/parcel2mirror.mat -interp trilinear'
+        #subprocess.run(bash_cmd.split(), check = True)
         print(f"Registered {rp}")
 
 
@@ -156,6 +171,6 @@ def register_parcels(sub, parcel_dir, parcels):
 for ss in enumerate(subj_list):
     
     #create_mirror_brain(ss)
-    register_mni(ss)
-    register_funcs(ss,exps)
+    #register_mni(ss)
+    #register_funcs(ss,exps)
     register_parcels(ss, f'{parcel_root}/{parcel_type}', parcels)
