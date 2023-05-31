b291c6055980d0d217d6430128312a675c6c9310
vayzenb <vayzenberg90@gmail.com>
2023-05-31 10:52:12 -0400

    added decoding and glass brains

--- analyses/cat_decoding.py
+++ analyses/cat_decoding.py
@@ -14,72 +14,107 @@ import hemispace_params as params
 from sklearn.linear_model import RidgeCV
 #import stratified shuffle split
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVC
 
 #task = sys.argv[1]
 #roi = sys.argv[2]
 
-task = 'spaceloc'
-roi = 'dorsal_visual_cortex'
 
 folds = 20
 test_size = 0.2
 
 
+results_dir = params.results_dir
 
-if task == 'spaceloc':
-    target = 'space'
-    distract = 'feat'
-elif task == 'tooloc':
-    target = 'tool'
-    distract = 'non_tool'
-elif task == 'face':
-    target = 'face'
-    distract = 'object'
-elif task == 'word':
-    target = 'word'
-    distract = 'object'
-
-
-all_subs = params.sub_info['sub'].values
-
-all_subs = ['sub-spaceloc1008']
-for sub, group, hemi in zip(params.sub_info['sub'],params.sub_info['group'], params.sub_info['intact_hemi']):
+task_info = params.task_info
+def lookup_task_info(cond):
     
-    mvpa_dir = f'{params.data_dir}/{sub}/ses-01/derivatives/mvpa'
+    if cond == 'space':
+        target = 'space'
+        distract = 'feat'
+        roi = 'dorsal_visual_cortex'
+    elif cond == 'tool':
+        target = 'tool'
+        distract = 'non_tool'
+        roi = 'dorsal_visual_cortex'
+    elif cond == 'face':
+        target = 'face'
+        distract = 'object'
+        roi = 'ventral_visual_cortex'
+    elif cond == 'word':
+        target = 'word'
+        distract = 'object'
+        roi = 'ventral_visual_cortex'
 
-    #load target and distract data
-    target_data = np.load(f'{mvpa_dir}/{roi}_{task}_{target}.npy')
-    distract_data = np.load(f'{mvpa_dir}/{roi}_{task}_{distract}.npy')
-
-    #append target and distract data
-    data = np.concatenate((target_data, distract_data))
-
-    #create labels with 1s for target and 0s for distract
-    labels = np.concatenate((np.ones(target_data.shape[0]), np.zeros(distract_data.shape[0])))
-
-    #set up cross validation
-    X = data
-    y = labels
-    
-    sss = StratifiedShuffleSplit(n_splits=folds, test_size=test_size, random_state=0)
-    sss.get_n_splits(X, y)
+    return target, distract, roi
 
 
-    roi_acc = []
-    for train_index, test_index in sss.split(X, y):
 
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
+summary_df = pd.DataFrame(columns = ['sub', 'group', 'hemi', 'roi','cond', 'acc', 'se'])
+for task,cond in zip(task_info['task'], task_info['cond']):
 
-        #pdb.set_trace()
-        clf = RidgeCV()
-        clf.fit(X_train, y_train)   
+    print(f'Running {task} {cond}...')
+    target, distract, roi = lookup_task_info(cond) #get target, distract, and roi for task
+    #loop through all subs
+    for sub, group, hemi in zip(params.sub_info['sub'],params.sub_info['group'], params.sub_info['intact_hemi']):
+        
+        mvpa_dir = f'{params.data_dir}/{sub}/ses-01/derivatives/mvpa'
+        
+        if hemi == 'both':
+            hemis = ['left','right']
+        else:
+            hemis = [hemi]
+        
+        for hemi in hemis:
 
-        roi_acc.append(clf.score(X_test, y_test))
-        #print(clf.score(X_test, y_test))
+            #check if task and cond exist in mvpa dir
+            if os.path.exists(f'{mvpa_dir}/{hemi}_{roi}_{task}_{target}.npy'):    
+                #print(f'Running {sub} {hemi}_{roi}_{task}_{target} vs {distract}...')            
 
-    print(f'{sub} {roi} {task} {np.mean(roi_acc)}')
+                #load target and distract data
+                target_data = np.load(f'{mvpa_dir}/{hemi}_{roi}_{task}_{target}.npy')
+                distract_data = np.load(f'{mvpa_dir}/{hemi}_{roi}_{task}_{distract}.npy')
 
+                #append target and distract data
+                data = np.concatenate((target_data, distract_data))
+
+                #remove columns with nans
+                data = data[:,~np.isnan(data).any(axis=0)]
+
+                #create labels with 1s for target and 0s for distract
+                labels = np.concatenate((np.ones(target_data.shape[0]), np.zeros(distract_data.shape[0])))
+
+                #set up cross validation
+                X = data
+                y = labels
+                
+                sss = StratifiedShuffleSplit(n_splits=folds, test_size=test_size, random_state=0)
+                sss.get_n_splits(X, y)
+
+
+                roi_acc = []
+                for train_index, test_index in sss.split(X, y):
+
+                    X_train, X_test = X[train_index], X[test_index]
+                    y_train, y_test = y[train_index], y[test_index]
+
+                    #pdb.set_trace()
+                    clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+                    clf.fit(X_train, y_train)   
+
+                    roi_acc.append(clf.score(X_test, y_test))
+                    #print(clf.score(X_test, y_test))
+
+                #add data to summary df using concat
+                summary_df = pd.concat([summary_df, pd.DataFrame([[sub, group, hemi, roi, cond, np.mean(roi_acc), np.std(roi_acc)]], columns = ['sub', 'group', 'hemi', 'roi','cond', 'acc', 'se'])])
+                
+                
+
+                #print(f'{sub} {hemi} {roi} {task} {np.mean(roi_acc)}')
+
+            summary_df.to_csv(f'{results_dir}/hemispace_decoding.csv', index=False)
 
 
 
